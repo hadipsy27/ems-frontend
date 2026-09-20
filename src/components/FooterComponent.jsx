286d51ccfac891de6531ea668b77135167937f64
Hadi Prasetyo <33802468+hadipsy27@users.noreply.github.com>
2026-09-20 17:20:35 +0700

    Add FooterComponent

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span>All righs reversed 2026 by chosakushin</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
